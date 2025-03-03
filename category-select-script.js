2fdca208d7c659f2fe9a0794cfd4e96bc5e304ef
DonovanCoT <donovan.thomas@myokanagan.bc.ca>
2025-03-03 07:58:50 -0800

    created 'tag select' pages for the musician and catering categories, aswell as changed the color scheme of all pages

--- category-select-script.js
+++ category-select-script.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.getElementById('foodTruckSearch');
+    const foodtruck = document.getElementById('foodTruckSearch');
+	const musicians = document.getElementById('musicianSearch');
+	const catering = document.getElementById('cateringSearch');
+
     
-    button.addEventListener('click', () => {
+    foodtruck.addEventListener('click', () => {
         window.location.href = "foodTruck-search-HTML.html";
+    });
+	
+	musicians.addEventListener('click', () => {
+        window.location.href = "musicians-search-HTML.html";
+    });
+	
+	catering.addEventListener('click', () => {
+        window.location.href = "catering-search-HTML.html";
     });
 });
