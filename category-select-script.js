47c8d60e631b941bf1912a5333a85f52ce4d1d61
nalydnitram <dylanpetermartin@gmail.com>
2025-03-06 10:02:03 -0800

    Update category-select-script.js

--- category-select-script.js
+++ category-select-script.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const foodtruck = document.getElementById('foodTruckSearch');
 	const musicians = document.getElementById('musicianSearch');
 	const catering = document.getElementById('cateringSearch');
-
-    
+    const accomodations = document.getElementById('accomodationSearch');
+    const transport = document.getElementById('transportationSearch');
+	const venues = document.getElementById('venueSearch');
+	
     foodtruck.addEventListener('click', () => {
         window.location.href = "foodTruck-search-HTML.html";
     });
@@ -15,4 +17,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	catering.addEventListener('click', () => {
         window.location.href = "catering-search-HTML.html";
     });
+	
+	transport.addEventListener('click', () => {
+    window.location.href = "TransportSearch.html";
+	});
+	
+	accomodations.addEventListener('click', () => {
+    window.location.href = "AccomodationsSearch.html";
+	});
+	
+	venues.addEventListener('click', () => {
+    window.location.href = "VenuesSearch.html";
+	});
 });
