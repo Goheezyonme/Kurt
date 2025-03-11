923bc26c9a3198f6c04b12d7ab58b5698f152d0e
DonovanCoT <donovan.thomas@myokanagan.bc.ca>
2025-03-11 11:05:15 -0700

    Created the basic dropdown page with generic registration forms. Customization to come

---
+++ registration-script.js
@@ -0,0 +1,53 @@
+        function toggleDropdownVenue() {
+            var content = document.getElementById("dropdownContentVenue");
+            var arrow = document.getElementById("arrowVenue");
+
+            content.classList.toggle("show");
+            arrow.classList.toggle("arrow-down");
+            arrow.classList.toggle("arrow-right");
+        }
+		
+		function toggleDropdownEntertainer() {
+            var content = document.getElementById("dropdownContentEntertainer");
+            var arrow = document.getElementById("arrowEntertainer");
+
+            content.classList.toggle("show");
+            arrow.classList.toggle("arrow-down");
+            arrow.classList.toggle("arrow-right");
+        }
+		
+		function toggleDropdownFoodTruck() {
+            var content = document.getElementById("dropdownContentFoodTruck");
+            var arrow = document.getElementById("arrowFoodTruck");
+
+            content.classList.toggle("show");
+            arrow.classList.toggle("arrow-down");
+            arrow.classList.toggle("arrow-right");
+        }
+		
+		function toggleDropdownCatering() {
+            var content = document.getElementById("dropdownContentCatering");
+            var arrow = document.getElementById("arrowCatering");
+
+            content.classList.toggle("show");
+            arrow.classList.toggle("arrow-down");
+            arrow.classList.toggle("arrow-right");
+        }
+		
+		function toggleDropdownTransportation() {
+            var content = document.getElementById("dropdownContentTransportation");
+            var arrow = document.getElementById("arrowTransportation");
+
+            content.classList.toggle("show");
+            arrow.classList.toggle("arrow-down");
+            arrow.classList.toggle("arrow-right");
+        }
+		
+		function toggleDropdownAccomodations() {
+            var content = document.getElementById("dropdownContentAccomodations");
+            var arrow = document.getElementById("arrowAccomodations");
+
+            content.classList.toggle("show");
+            arrow.classList.toggle("arrow-down");
+            arrow.classList.toggle("arrow-right");
+        }
