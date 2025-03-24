7ba01ff3dc6b326bad75c3de6dfd781211560f4a
Goheezyonme <90871025+Goheezyonme@users.noreply.github.com>
2025-03-24 12:12:49 -0700

    Update Signup.php

--- Signup.php
+++ Signup.php
@@ -3,7 +3,7 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "mysql";
-$dbname = "user_signups";
+$dbname = "isotalent";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
